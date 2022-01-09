6bc7c7a639020f9d654bec13e0264290c6c09759
riku <riku4724@gmail.com>
2022-01-10 08:53:15 +0900

    add message controller

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # api test action
       resources :hello, only:[:index]
+      post '/callback' => 'message#callback'
     end
   end
 end
